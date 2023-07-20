f9fc35570936e0a0893b3fac963d550e6ce6d036
Alik <alikharutyunyan00@gmail.com>
2023-07-20 03:30:27 +0300

    solution 3 finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Counter counter = new Counter();
         Object lock = new Object();
 
